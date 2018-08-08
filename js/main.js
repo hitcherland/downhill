bc3b4b8307dc0a3bea42b67c408f985f757ee592
Cian Booth <thisiscian@gmail.com>
2018-08-08 16:28:01 +0100

    changed to scrolling mountains and a fun pseudo intro

--- js/main.js
+++ js/main.js
@@ -1,79 +1,102 @@
-var background;
+var background, mountains, text, subtext;
+var globalSpeed = 0.00001;
 
-function preload () { }
+function preload () { 
+    game.time.advancedTiming = true;
+}
 
 function create ()
 {
-    background = new Background();
+    mountains = [ 
+        new Mountain( 0x333333, game.height * 0.1, game.height * 0.4, 0.01, 0.05, 1),
+        new Mountain( 0x666666, game.height * 0.3, game.height * 0.6, 0.01, 0.01, 2),
+        new Mountain( 0x999999, game.height * 0.7, game.height * 0.9, 0.1, 0.01, 4 )
+    ];
+
+    text = game.add.text( 0, 0, "MOUNTAINS", { 
+        "font": "5vw sans-serif", 
+        "boundsAlignH": "center",
+        "boundsAlignV": "middle",
+        "strokeThickness": 4,
+        "stroke": "#ffffff80",
+    } );
+    text.centerX = game.width / 2;
+    text.centerY = game.height / 2;
+
+    text2 = game.add.text( 0, 0, "- trouble writ in stone -", { 
+        "font": "2.1vw monospace", 
+        "boundsAlignH": "center",
+        "boundsAlignV": "middle",
+        "strokeThickness": 4,
+        "stroke": "#ffffff80",
+    } );
+    text2.centerX = game.width / 2;
+    text2.top = text.bottom - text.height * 0.1;
 }
 
 function update() { 
-    background.update();
+    game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
+    mountains.map( x=> x.update() );
 }
 
-class Background {
-    constructor() {
-        this.xscale = 300;
-        this.yscale = 10;
+class Mountain {
+    constructor( color, minLimit, maxLimit, hResolution, vResolution, speed ) {
+        this.hResolution = game.width * hResolution || 1;
+        this.vResolution = vResolution || 0.01;
+        this.color = color;
+        this.speed = speed * game.width || 0.01;
+        this.minLimit = minLimit;
+        this.maxLimit = maxLimit;
+   
         this.graphics = game.add.graphics(0, 0);
-        this.beziers = [];
-        this.true = [];
-        var X, x, y = game.height / 2.0;
-        var dx = game.width / this.xscale;
-        for( var i=dx; i<game.width; i+=dx) {
-            Array.prototype.push.apply( this.beziers, [
-                (i - dx/2), y - this.yscale,
-                (i - dx/2), y - this.yscale,
-                i, y
-            ]);
-            Array.prototype.push.apply( this.true, [
-                (i - dx/2), y - this.yscale,
-                (i - dx/2), y - this.yscale,
-                i, y
-            ]);
+        this.points = [ game.width, game.height, 0, game.height ];
+        this.height = game.rnd.between( minLimit, maxLimit )
+        this.steepness = game.rnd.between( -game.height * this.vResolution, game.height * this.vResolution );
 
+        Array.prototype.push.apply( this.points,  );
+        for( var i=0; i<game.width + this.hResolution; i+= this.hResolution) {
+            this.addPoint( i );
         }
-        Array.prototype.push.apply( this.beziers, [
-            (game.width-dx/2), y - this.yscale,
-            (game.width-dx/2), y - this.yscale,
-            game.width, y
-        ]);
-        Array.prototype.push.apply( this.true, [
-            (game.width-dx/2), y - this.yscale,
-            (game.width-dx/2), y - this.yscale,
-            game.width, y
-        ]);
-
         this.draw();
     }
 
+
+    addPoint( x ) {
+        this.steepness = game.rnd.between( -game.height * this.vResolution, game.height * this.vResolution );
+        Array.prototype.push.apply( this.points, [ x, this.height ] );
+        this.height += this.steepness;
+        if ( this.height < this.minLimit ) {
+            this.height = 2 * this.minLimit - this.height;
+        }
+
+        if ( this.height >= this.maxLimit ) {
+            this.height = 2 * this.maxLimit - this.height;
+        }
+    }
+
+
     draw() {
         this.graphics.clear();
-        this.graphics.beginFill(0xFF33ff);
-        this.graphics.moveTo( game.width, game.height/2 );
-        this.graphics.lineTo( game.width, game.height );
-        this.graphics.lineTo( 0, game.height );
-        this.graphics.lineTo( 0, game.height/2 );
-        for( var i=0; i<this.beziers.length; i+=6 ) {
-            var bezier = this.beziers.slice( i, i+6 );
-            this.graphics.bezierCurveTo( ...bezier );
-        }
+        this.graphics.beginFill( this.color );
+        this.graphics.drawPolygon( this.points );
         this.graphics.endFill();
     }
 
     update() {
-        var dx = game.width / ( 2 *this.xscale ) ;
-        for( var i=0; i<this.beziers.length; i+=6 ) {
-            var b = this.true[ i ];
-            var min = b - dx;
-            var max = b + dx;
-            var B = this.beziers[ i ] + game.rnd.between( -2,2 ); 
-            if( B < min )
-                B = min;
-            if( B > max )
-                B = max;
-            this.beziers[ i ] = B;
-            this.beziers[ i + 2 ] = B;
+        var l = this.points.length;
+        var cull = 0;
+        for( var i = 4; i < l; i+= 2 ) {
+            this.points[ i ] -= game.time.elapsed * this.speed * globalSpeed;
+            if( this.points[ i ] < -this.hResolution ) {
+                cull = i - 4;
+            }
+        }
+        var end = this.points[ l - 2 ];
+        this.points.splice( 4, cull );
+        var i = 1;
+        while( end + this.hResolution * i < game.width + this.hResolution ) {
+            this.addPoint( end + this.hResolution * i );
+            i++;
         }
         this.draw();
     }
