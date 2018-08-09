4720d58997f557e76f33c80981558d0a069c0a36
Cian Booth <this.is.cian@gmail.com>
2018-08-09 14:19:56 +0100

    added in resize ability

--- js/main.js
+++ js/main.js
@@ -2,34 +2,43 @@ var background, mountains, text, subtext;
 var globalSpeed = 0.00001;
 
 function preload () { 
+	game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
     game.time.advancedTiming = true;
 }
 
 function create ()
 {
     mountains = [ 
-        new Mountain( 0x333333, game.height * 0.1, game.height * 0.4, 0.01, 0.05, 1),
-        new Mountain( 0x666666, game.height * 0.3, game.height * 0.6, 0.01, 0.01, 2),
-        new Mountain( 0x999999, game.height * 0.7, game.height * 0.9, 0.1, 0.01, 4 )
+        new Mountain({ "color": 0x333333, 
+						"minLimit": 0.1,
+						"maxLimit": 0.4,
+						"hResolution": 0.01, 
+						"vResolution": 0.05,
+						"speed": 1
+		}),
+        new Mountain( 0x666666, 0.3, 0.6, 0.01, 0.01, 2),
+        new Mountain( 0x999999, 0.7, 0.9, 0.1, 0.01, 4 )
     ];
 
     text = game.add.text( 0, 0, "MOUNTAINS", { 
-        "font": "5vw sans-serif", 
+        "font": "5vmin sans-serif", 
         "boundsAlignH": "center",
         "boundsAlignV": "middle",
         "strokeThickness": 4,
         "stroke": "#ffffff",
     } );
-    text.centerX = game.width / 2;
-    text.centerY = game.height / 2;
 
     text2 = game.add.text( 0, 0, "- trouble writ in stone -", { 
-        "font": "2.1vw monospace", 
+        "font": "2.1vmin monospace", 
         "boundsAlignH": "center",
         "boundsAlignV": "middle",
         "strokeThickness": 3,
         "stroke": "#ffffff",
     } );
+
+    text.centerX = game.width / 2;
+    text.centerY = game.height / 2;
+
     text2.centerX = game.width / 2;
     text2.top = text.bottom - text.height * 0.1;
 }
@@ -39,31 +48,53 @@ function update() {
     mountains.map( x=> x.update() );
 }
 
+function resize() {
+    mountains.map( x=> x.resize() );
+	text.updateText();
+	text2.updateText();
+	text.centerX = game.width / 2;
+	text.centerY = game.height / 2;
+
+	text2.centerX = game.width / 2;
+	text2.top = text.bottom - text.height * 0.1;
+
+}
+
 class Mountain {
     constructor( color, minLimit, maxLimit, hResolution, vResolution, speed ) {
-        this.hResolution = game.width * hResolution || 1;
+		if( typeof( color ) == "object" ) {
+			hResolution = color.hResolution;
+			vResolution = color.vResolution;
+			speed = color.speed;
+			minLimit = color.minLimit;
+			maxLimit = color.maxLimit;
+			color = color.color;
+		}	
+        this.hResolution = hResolution || 0.01;
         this.vResolution = vResolution || 0.01;
-        this.color = color;
-        this.speed = speed * game.width || 0.01;
-        this.minLimit = minLimit;
-        this.maxLimit = maxLimit;
+        this.color = color || 0xff00ff;
+        this.speed = speed || 0.01;
+        this.minLimit = minLimit || 0;
+        this.maxLimit = maxLimit || 1;
    
         this.graphics = game.add.graphics(0, 0);
-        this.points = [ game.width, game.height, 0, game.height ];
-        this.height = game.rnd.between( minLimit, maxLimit )
-        this.steepness = game.rnd.between( -game.height * this.vResolution, game.height * this.vResolution );
+        this.relPoints = [ 1, 1, 0, 1 ];
+        this.height = game.rnd.realInRange( this.minLimit, this.maxLimit )
+        this.steepness = game.rnd.realInRange( -this.vResolution, this.vResolution );
 
-        Array.prototype.push.apply( this.points,  );
-        for( var i=0; i<game.width + this.hResolution; i+= this.hResolution) {
+        for( var i=0; i< 1 + this.hResolution; i+= this.hResolution) {
             this.addPoint( i );
         }
         this.draw();
     }
 
+	updatePointsFromRel() {
+		this.points = this.relPoints.map( ( x, i ) => i % 2 == 0 ? x * game.width : x * game.height );
+	}
 
     addPoint( x ) {
-        this.steepness = game.rnd.between( -game.height * this.vResolution, game.height * this.vResolution );
-        Array.prototype.push.apply( this.points, [ x, this.height ] );
+        this.steepness = game.rnd.realInRange( -this.vResolution, this.vResolution );
+        Array.prototype.push.apply( this.relPoints, [ x, this.height ] );
         this.height += this.steepness;
         if ( this.height < this.minLimit ) {
             this.height = 2 * this.minLimit - this.height;
@@ -74,8 +105,8 @@ class Mountain {
         }
     }
 
-
     draw() {
+		this.updatePointsFromRel();
         this.graphics.clear();
         this.graphics.beginFill( this.color );
         this.graphics.drawPolygon( this.points );
@@ -83,21 +114,25 @@ class Mountain {
     }
 
     update() {
-        var l = this.points.length;
+        var l = this.relPoints.length;
         var cull = 0;
         for( var i = 4; i < l; i+= 2 ) {
-            this.points[ i ] -= game.time.elapsed * this.speed * globalSpeed;
-            if( this.points[ i ] < -this.hResolution ) {
+            this.relPoints[ i ] -= game.time.elapsed * this.speed * globalSpeed;
+            if( this.relPoints[ i ] < -this.hResolution ) {
                 cull = i - 4;
             }
         }
-        var end = this.points[ l - 2 ];
+        var end = this.relPoints[ l - 2 ];
         this.points.splice( 4, cull );
         var i = 1;
-        while( end + this.hResolution * i < game.width + this.hResolution ) {
+        while( end + this.hResolution * i < 1 + this.hResolution ) {
             this.addPoint( end + this.hResolution * i );
             i++;
         }
         this.draw();
     }
+
+	resize() {
+        this.draw();
+	}
 }
